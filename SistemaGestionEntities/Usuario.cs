e2e82c3538740a7756d8408ee75571218c685426
Igniske <104842998+Igniske@users.noreply.github.com>
2024-02-12 18:19:40 -0300

    Add files via upload

---
+++ SistemaGestionEntities/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionEntities
+{
+    public class Usuario
+    {
+        private int _id;
+        private string _nombre;
+        private string _apellido;
+        private string _nombreUsuario;
+        private string _contrasenia;
+        private string _mail;
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contrasenia { get; set; }
+        public string Mail { get; set; }
+    }
+}
